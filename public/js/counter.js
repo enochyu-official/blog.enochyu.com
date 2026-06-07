e73851c4854396d8294388ad7021331b4f7402d3
Enoch Yu <247741827+enochyu-official@users.noreply.github.com>
2026-06-07 18:34:12 +0900

    view count error3

--- public/js/counter.js
+++ public/js/counter.js
@@ -5,10 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
       const response = await fetch(`https://blog-view-count-db.enochyu.workers.dev/?name=${blogName}`);
       const text = await response.text();
       
-      span.textContent = text.match(/\d+$/)?.[0] || "0";
+      span.textContent = text.match(/\d+$/)?.[0];
     } catch {
       span.textContent = "—";
     }
   });
 });
+
 
